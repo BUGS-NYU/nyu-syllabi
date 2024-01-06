cb61bfce9590bf8fc9172fad3979b73a980fd9f8
Aneesh Maganti <28660350+aminoa@users.noreply.github.com>
2024-01-06 11:12:11 -0500

    current changes, still more to do

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -3,19 +3,51 @@ import Link from 'next/link';
 import { schools } from '../../data/schools';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import Form from '../../components/form';
+import supabase from '@/utils/supabase';
+
+type Props = {
+    schools: any,
+    uploadSyllabi: any
+}
+
+// function ValidateForm(course_code:string, course_name:string, schools_form:string, term:string, year:string, file:File) {
+// 	const course_code_validation = '^[A-Z]+-[A-Z]+-[A-Z0-9]+$'
+// 	const school_validation = "^(" + schools.map((school:any) => school.name).join("|") + ")$"
+// 	const term_validation = "^(" + ["Fall", "Spring", "Summer", "Winter"].join("|") + ")$"
+
+// 	if (!course_code.match(course_code_validation) 
+// 	|| !schools_form.match(school_validation) 
+// 	|| !term.match(term_validation) 
+// 	|| (1990 < parseInt(year) && parseInt(year) < 2030)
+// 	|| (!file.name.endsWith('.pdf')) 
+// 	|| (file.size > 10000000)) {
+// 		return false;
+// 	}
+// 	return true;
+// }
 
 export default function Upload() {
-	const course_code_validation = '^[A-Z]+-[A-Z]+-[A-Z0-9]+$'
-
 	const uploadSyllabi = async (formData: FormData) => {
-		"use server";
-		console.log(formData);
-		const course_code = formData.get("course-code");
-		const title = formData.get("title");
-		const school = formData.get("school");
-		const term = formData.get("term");
-		const year = formData.get("year");
-		const link = formData.get("link");
+		//verify no null for all fields
+		if (!formData.get('course_code') || !formData.get('course_name') || !formData.get('schools_form') || !formData.get('term_form') || !formData.get('year_form') || !formData.get('file_form')) {
+			// alert("Please fill out all fields");
+			return false;
+		}
+
+		const course_code = formData.get('course_code') as string;
+		const course_name = formData.get('course_name') as string;
+		const schools_form = formData.get('schools_form') as string;
+		const term_form = formData.get('term_form') as string;
+		const year_form = formData.get('year_form') as string
+		const file_form = formData.get('file_form') as File;
+
+		// try () {
+		// 	ValidateForm(course_code, course_name, schools_form, term_form, year_form, file_form) == false
+		// 	console.log(course_code, course_name, schools_form, term_form, year_form, file_form);
+		// } catch (e) {
+
+		// }
+
 	};
 
 	return (
--- components/form.tsx
+++ components/form.tsx
@@ -7,7 +7,8 @@ type Props = {
 
 {/* course-code (check regex check, copy from google sheets), title, school (drop-down), term (drop-down), year, upload doc (cap to 10 mb) */}
 const Form = ({ schools, uploadSyllabi } : Props) => {
-    const course_code_validation = '^[A-Z]+-[A-Z]+-[A-Z0-9]+$'
+    // const course_code_validation = '^[A-Z]+-[A-Z]+-[A-Z0-9]+$'
+    const course_code_validation = '^[A-Za-z]+-[A-Za-z]+-[A-Za-z0-9]+$'
     const school_validation = "^(" + schools.map((school:any) => school.name).join("|") + ")$"
     const term_validation = "^(" + ["Fall", "Spring", "Summer", "Winter"].join("|") + ")$"
 
@@ -30,6 +31,7 @@ const Form = ({ schools, uploadSyllabi } : Props) => {
                     <input 
                         type="text" 
                         id="schools_form" 
+                        name='schools_form'
                         list="schools_form_listings" 
                         placeholder='School (ex. College of Arts and Science)' 
                         pattern={school_validation}
@@ -46,6 +48,7 @@ const Form = ({ schools, uploadSyllabi } : Props) => {
                 <li>
                     <input 
                         type="text" 
+                        name='term_form'
                         id="term_form" 
                         list="term_form_listings" 
                         placeholder='Term (ex. Fall)' 
@@ -61,7 +64,7 @@ const Form = ({ schools, uploadSyllabi } : Props) => {
                     </datalist>
                 </li>
 
-                <li> <input type="number" id="year_form" name="year_form" placeholder='Year (ex. 2022)' min={1990} max={2030} /> </li>
+                <li> <input type="number" id="year_form" name="year_form" placeholder='Year (ex. 2022)' min={1990} max={2030} required /> </li>
                 <li id='filesubmit'> 
                     <input type="file" id="file_form" name="file_form" accept=".pdf, .doc, .docx" /> 
                     <input type="submit" value="Submit" /> 
