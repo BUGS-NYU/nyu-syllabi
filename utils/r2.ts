106b54431322700a1743dfd54a293fcc34a67bdb
Aneesh Maganti <28660350+aminoa@users.noreply.github.com>
2024-03-24 23:55:26 -0400

    add error check

--- utils/r2.ts
+++ utils/r2.ts
@@ -7,6 +7,10 @@ const ACCESS_KEY_ID =  process.env.R2_ACCESS_KEY_ID;
 const SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
 const R2_UPLOAD_BUCKET = process.env.R2_UPLOAD_BUCKET
 
+if (!R2_ACCOUNT_ID || !ACCESS_KEY_ID || !SECRET_ACCESS_KEY || !R2_UPLOAD_BUCKET) {
+  throw new Error("Missing R2_ACCOUNT_ID, R2_ACCESS_KEY_ID, R2_SECRET_ACCESS_KEY, or R2_UPLOAD_BUCKET environment variable")
+}
+
 const s3 = new S3Client({
   region: 'auto',
   endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
